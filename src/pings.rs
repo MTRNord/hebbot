26c2cd777dd0e048208c4a5a7dfb218ca4fb08dc
MTRNord <MTRNord@users.noreply.github.com>
2026-05-05 11:59:43 +0200

    reduce one allocation and add unit test
    Signed-off-by: MTRNord <MTRNord@users.noreply.github.com>

--- src/pings.rs
+++ src/pings.rs
@@ -35,26 +35,73 @@ struct Score {
 pub struct Pings {}
 
 impl Pings {
+    fn parse_pings(ping_data: Response) -> Vec<Score> {
+        let mut sorted_pings: Vec<Score> = ping_data
+            .pings
+            .iter()
+            .map(|(k, v)| Score {
+                server: k.to_string(),
+                median: v.median,
+            })
+            .collect();
+        sorted_pings.sort_by(|a, b| a.median.total_cmp(&b.median));
+        sorted_pings
+    }
+
     pub fn get(room_id: &str) -> String {
-        let ping_json = reqwest::blocking::get(format!(
+        let ping_data: Response = reqwest::blocking::get(format!(
             "https://maubot.xyz/_matrix/maubot/plugin/pingstat/{room_id}/stats.json"
         ))
         .expect("request should succeed")
-        .text()
-        .expect("response should have text");
-        let ping_data: Response = serde_json::from_str(&ping_json).expect("should parse as JSON");
-        let mut sorted_pings: Vec<Score> = Vec::new();
-        for (k, v) in ping_data.pings.iter() {
-            sorted_pings.push(Score {
-                server: k.to_string(),
-                median: v.median,
-            });
-        }
-        sorted_pings.sort_by(|a, b| a.median.total_cmp(&b.median));
+        .json()
+        .expect("request should be parseable into our response format");
+
+        let sorted_pings = Pings::parse_pings(ping_data);
         let mut table = String::from("");
         for (i, score) in sorted_pings[0..10].iter().enumerate() {
             table = format!("{table}|{}|{}|{}|\n", i + 1, score.server, score.median);
         }
         table
+    }
+}
+
+// Test cases specific to pings
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ensure_sorted() {
+        let ping_data = Response {
+            disclaimer: String::new(),
+            pings: HashMap::from([
+                (
+                    "server1".to_string(),
+                    Ping {
+                        pongs: None,
+                        pings: vec![],
+                        mean: 0f32,
+                        median: 10f32,
+                        gmean: 0f32,
+                    },
+                ),
+                (
+                    "server2".to_string(),
+                    Ping {
+                        pongs: None,
+                        pings: vec![],
+                        mean: 0f32,
+                        median: 5f32,
+                        gmean: 0f32,
+                    },
+                ),
+            ]),
+            mean: 0f32,
+            pongservers: Vec::new(),
+        };
+
+        let sorted_pings = Pings::parse_pings(ping_data);
+        assert_eq!(sorted_pings.len(), 2);
+        assert!(sorted_pings[0].median <= sorted_pings[1].median);
     }
 }
